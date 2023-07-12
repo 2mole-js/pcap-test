7248401fc98d6934a7b144c20e72a46c03b27de3
scarecr0ws <djisu606@bu.ac.kr>
2023-07-12 06:38:43 -0400

    fix payloaddata print

--- pcap-test.c
+++ pcap-test.c
@@ -4,9 +4,10 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>				//for IPPROTO_TCP socket
+#define LIBNET_LIL_ENDIAN			//for use th_off; in Little_Endian
 #define ETHER_ADDR_LEN 6
 #define ETHERTYPE_IP 0x0800
-#define PAYLOAD_DATA_LEN 10
+#define PAYLOAD_DATA_MAX_LEN 65535
 
 /*
  *  Ethernet II header
@@ -26,9 +27,10 @@ struct libnet_ethernet_hdr
  */
 struct libnet_ipv4_hdr
 {
-#if (LIBNET_LIL_ENDIAN)
-    u_int8_t ip_hl:4,      /* header length */
-           ip_v:4;         /* version */
+#if defined(LIBNET_LIL_ENDIAN)
+    u_int8_t ip_hl:4;        /* header length */
+    u_int8_t ip_v:4;         /* version */
+
 #endif
 #if (LIBNET_BIG_ENDIAN)
     u_int8_t ip_v:4,       /* version */
@@ -79,9 +81,10 @@ struct libnet_tcp_hdr
     u_int16_t th_dport;       /* destination port */
     u_int32_t th_seq;          /* sequence number */
     u_int32_t th_ack;          /* acknowledgement number */
-#if (LIBNET_LIL_ENDIAN)
-    u_int8_t th_x2:4,         /* (unused) */
-           th_off:4;        /* data offset */
+#if defined(LIBNET_LIL_ENDIAN)
+    u_int8_t th_x2:4;        /* (unused) */
+    u_int8_t th_off:4;       /* data offset */
+
 #endif
 #if (LIBNET_BIG_ENDIAN)
     u_int8_t th_off:4,        /* data offset */
@@ -117,6 +120,10 @@ struct libnet_tcp_hdr
     u_int16_t th_urp;         /* urgent pointer */
 };
 
+struct libnet_Payload_hdr{
+	u_int8_t  payload_Data[PAYLOAD_DATA_MAX_LEN];
+};
+
 void printMac(u_int8_t* m) {
 	printf("%02x:%02x:%02x:%02x:%02x:%02x",m[0],m[1],m[2],m[3],m[4],m[5]);
 }
@@ -129,15 +136,8 @@ void print_IP(struct in_addr *ip) {
 void print_port(u_int16_t m){
 	printf("%d", ntohs(m));
 }
-void print_pldata(u_int8_t* Payload_Data){
-	int check_zero = 0;
-	for (int i = 0; i < 10; i++) {
-        if (Payload_Data[i] != 0x00) {
-            check_zero=1;
-            break;
-        }
-    }
-    if (check_zero == 0){
+void print_pldata(u_int8_t* Payload_Data, int payload_data_LEN){
+    if (payload_data_LEN == 0){
     	printf("ZERO DATA");
     	return;
     }
@@ -194,7 +194,9 @@ int main(int argc, char* argv[]) {
 		struct libnet_ethernet_hdr* eth_hdr=(struct libnet_ethernet_hdr *)packet;
 		struct libnet_ipv4_hdr* ipv4_hdr=(struct libnet_ipv4_hdr *)(packet+sizeof(struct libnet_ethernet_hdr));
 		struct libnet_tcp_hdr* tcp_hdr=(struct libnet_tcp_hdr *)(packet+sizeof(struct libnet_ethernet_hdr)+sizeof(struct libnet_ipv4_hdr));
-		uint8_t* Payload_Data = (uint8_t*)tcp_hdr + sizeof(struct libnet_tcp_hdr);
+		u_int16_t tcp_hdr_len = tcp_hdr->th_off * 4;
+		struct libnet_Payload_hdr*  pl_Data= (struct libnet_Payload_hdr*)(packet+sizeof(struct libnet_ethernet_hdr)+sizeof(struct libnet_ipv4_hdr)+tcp_hdr_len);
+		int payload_data_LEN=ntohs(ipv4_hdr->ip_len) - (ipv4_hdr->ip_hl * 4 + tcp_hdr->th_off * 4);
 		// TCP 소캣 전처리기를 이용하여 프로토콜 번호 6이 아닐경우 해당 패킷은 출력 안함
 		if (ipv4_hdr->ip_p != IPPROTO_TCP) {
     		printf("Not TCP packet\n");
@@ -217,8 +219,9 @@ int main(int argc, char* argv[]) {
 		print_port(tcp_hdr->th_sport);
 		printf("\n Tcp header Destination Port:\t");
 		print_port(tcp_hdr->th_dport);
-		printf("\n Playload Data(MAX 10 Byte) :\t");
-	    print_pldata(Payload_Data);
+		printf("\n payload_data_LEN:\t%d",payload_data_LEN);
+		printf("\n Payload Data(MAX 10 Byte) :\t");
+	    print_pldata(pl_Data->payload_Data, payload_data_LEN);
 	    printf("\n");
 	}
 
